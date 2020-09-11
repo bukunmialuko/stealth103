7941da4e8eaaae0fdc52c61914b08759913811eb
Bukunmi <oluwabukunmi.aluko@gmail.com>
2020-09-11 23:32:59 +0100

    .

--- src/com/example/stealth103/TennisCoach.java
+++ src/com/example/stealth103/TennisCoach.java
@@ -3,7 +3,7 @@ package com.example.stealth103;
 import com.example.stealth103.interfaces.ICoach;
 import org.springframework.stereotype.Component;
 
-@Component()
+@Component
 public class TennisCoach implements ICoach {
     @Override
     public String getDailyWorkout() {
