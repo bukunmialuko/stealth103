95215b4d6e4928d14f18564a0272f1cf874ff9e8
Bukunmi <oluwabukunmi.aluko@gmail.com>
2020-09-14 13:32:22 +0100

    Example of singlton scope with annotations

---
+++ src/com/example/stealth103/main/AnnotationBeanScopeDemoApp.java
@@ -0,0 +1,30 @@
+package com.example.stealth103.main;
+
+import com.example.stealth103.interfaces.ICoach;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationBeanScopeDemoApp {
+    public static void main(String[] args) {
+        // read spring config file
+        ClassPathXmlApplicationContext context = new
+                ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // get the bean from spring container
+        ICoach theCoach = context.getBean("tennisCoach", ICoach.class);
+        ICoach alphaCoach = context.getBean("tennisCoach", ICoach.class);
+
+        // call a method on the bean
+        boolean result = (theCoach == alphaCoach);
+
+        System.out.println("\nPointing to the same object: " + result);
+        System.out.println("\nMemory location for theCoach: " + theCoach);
+        System.out.println("\nMemory location for alphaCoach: " + alphaCoach);
+
+        System.out.println(theCoach.getDailyWorkout());
+        System.out.println(theCoach.getDailyFortune());
+
+        // close the context
+        context.close();
+
+    }
+}
